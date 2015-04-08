6919da4f2c928460c1796008f5ef0c58c3433d6e
newmanne <newman.n8@gmail.com>
2015-04-07 23:58:43 -0700

    Blah. Can't verify solutions because of finite fields...

--- rlnc.py
+++ rlnc.py
@@ -10,33 +10,34 @@ PULL = 1
 EXCHANGE = 2 
 BROADCAST = 3
 
-FIELD_SIZE = 2
-NUM_NODES = 100
+FIELD_SIZE = 10
+NUM_NODES = 2
 
 class RLNCNode:
 	def __init__(self, identifier):
 		self.messages = np.array([]) # A matrix where every row is a message
 		self.coefficients = np.array([]) # A matrix where every row is coefficients for a message
-		self.independent_coeffs = np.array([])
-		self.independent_messages = np.array([])
 		self.identifier = identifier
 
 	def receive(self, message):
-		old_rank = self.rank()
+		if np.all(message.coefficients==0):
+			return # Message is just the 0 vector
+		elif self.can_decode():
+			return # No need to receive anything
+
 		if self.messages.size == 0:
+			print message
 			self.coefficients = np.array([message.coefficients])
 			self.messages = np.array([message.message])
 		else:
-			self.coefficients = np.concatenate((self.coefficients, np.array([message.coefficients])), axis=0)
-			self.messages = np.concatenate((self.messages, np.array([message.message])), axis=0)
-		new_rank = self.rank()
-		if old_rank != new_rank: # Must have added a linearly independent equation!
-			if self.independent_coeffs.size == 0:
-				self.independent_coeffs = np.array([message.coefficients])
-				self.independent_messages = np.array([message.message])
-			else:
-				self.independent_coeffs = np.concatenate((self.independent_coeffs, np.array([message.coefficients])), axis=0)
-				self.independent_messages = np.concatenate((self.independent_messages, np.array([message.message])), axis=0)
+			old_rank = self.rank()
+			new_coefficients = np.concatenate((self.coefficients, np.array([message.coefficients])), axis=0)
+			new_messages = np.concatenate((self.messages, np.array([message.message])), axis=0)
+			new_rank = compute_rank(new_coefficients)
+			if new_rank > old_rank:
+				print message
+				self.coefficients = new_coefficients
+				self.messages = new_messages
 
 	def rank(self):
 		return compute_rank(self.coefficients)
@@ -45,23 +46,22 @@ class RLNCNode:
 		return self.rank() == NUM_MESSAGES
 
 	def get_random_message(self):
-		if self.messages.size == 0:
+		if self.messages.size == 0: # You have nothing to broadcast
 			return
-		n_seen_messages = self.messages.shape[0]
-		rand_coefficients = np.random.randint(0, FIELD_SIZE, n_seen_messages) # One random coefficient for each message I know about
-		message = np.zeros(MESSAGE_LENGTH)
-		for i in range(n_seen_messages):
-			message += self.messages[i] * rand_coefficients[i]
-		message = mod(message)
-		# get actual coefficients
+
+		rand_coefficients = np.random.randint(0, FIELD_SIZE, self.messages.shape[0]) # One random coefficient for each message I know about
+		message = mod(np.dot(rand_coefficients, self.messages))
 		coefficients = mod(np.dot(rand_coefficients, self.coefficients))
 		return RLNCMessage(coefficients, message)
 
 	def decode(self):
-		a = self.independent_coeffs
-		b = self.independent_messages
-		solution = np.absolute(mod(np.linalg.solve(a, b)))
-		print solution
+		a = self.coefficients
+		b = self.messages
+		assert a.shape == (NUM_MESSAGES, NUM_MESSAGES)
+		assert b.shape == (NUM_MESSAGES, MESSAGE_LENGTH)
+		solution = np.linalg.solve(a, b) #Not allowed to do this because of the whole finite field thing
+		solution = mod(solution)
+		return solution.astype(np.int)
 
 class RLNCMessage:
 	def __init__(self, coefficients, message):
@@ -69,9 +69,9 @@ class RLNCMessage:
 		assert message.shape == (MESSAGE_LENGTH, )
 		self.coefficients = coefficients
 		self.message = message
+
 	def __str__(self):
-		tostr = "coefficients:" + str(self.coefficients) + "message:" + str(self.message)
-		return tostr
+		return "coefficients:" + str(self.coefficients) + "message:" + str(self.message)
 
 def compute_rank(matrix):
 	return np.linalg.matrix_rank(matrix)
@@ -112,28 +112,37 @@ def send_message(sender, receiver):
 	if message is not None:
 		graph.node[receiver]['rlnc'].receive(message)
 
-# TODO: actually test that you can decode things?
+def verify_solution(solution, messages):
+	for message in messages:
+		assert message.tolist() in solution.tolist()
+
 # TODO: count number of messages sent?
 # TODO: loop for variance
 
 if __name__ == '__main__':
+	
 	# 1) Generate a connected graph
 	print "Generating graph"
 	graph = generate_complete_graph(NUM_NODES)
 	for node in graph.nodes():
 		graph.node[node]['rlnc'] = RLNCNode(node)
+
 	# 2) Create the messages
 	messages = [
 		np.array([1, 0,] * 5),
-		np.array([0] * 10),
+		np.array([1] * 10),
 		np.array([1] * 9 + [0])
 	]
 	NUM_MESSAGES = len(messages)
 	MESSAGE_LENGTH = len(messages[0])
+	for message in messages:
+		assert len(message) == MESSAGE_LENGTH
+
 	# 3) Distribute the messages
 	standard_basis_vectors = np.identity(NUM_MESSAGES)
 	for i, message in enumerate(messages):
-		graph.node[1]['rlnc'].receive(RLNCMessage(standard_basis_vectors[i], message))
+		graph.node[0]['rlnc'].receive(RLNCMessage(standard_basis_vectors[i], message))
+	
 	# 4) Run gossip until can stop
 	print "Starting protocol"
 	rounds = 0
@@ -144,6 +153,9 @@ if __name__ == '__main__':
 			summarize(graph)
 		print "It's now round %d" % (rounds)
 	print "It took %d rounds" % (rounds)
+
+	# 5) Verify answer
 	for node in graph.nodes():
-		graph.node[node]['rlnc'].decode()
-		break
+		solution = graph.node[node]['rlnc'].decode()
+		print solution
+		verify_solution(solution, messages)
