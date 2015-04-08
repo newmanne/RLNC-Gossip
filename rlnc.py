3838cd0f57e2c67e623c7dae9b8906ba21333597
newmanne <newman.n8@gmail.com>
2015-04-08 13:57:31 -0700

    Use sage to properly handle finite fields. Seems to work now

--- rlnc.py
+++ rlnc.py
@@ -1,17 +1,22 @@
+from sage.all import *
 import networkx as nx
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import random
-import binascii
 
 PUSH = 0
 PULL = 1
 EXCHANGE = 2 
 BROADCAST = 3
 
-FIELD_SIZE = 10
-NUM_NODES = 2
+FIELD_SIZE = 2 # MUST BE A PRIME OR PRIME POWER!
+NUM_NODES = 200
+NUM_MESSAGES = 10
+MESSAGE_LENGTH = 100
+
+COEFF_MATRIX_SPACE = MatrixSpace(GF(FIELD_SIZE), NUM_MESSAGES, NUM_MESSAGES)
+MESSAGE_MATRIX_SPACE = MatrixSpace(GF(FIELD_SIZE), NUM_MESSAGES, MESSAGE_LENGTH)
 
 class RLNCNode:
 	def __init__(self, identifier):
@@ -21,21 +26,20 @@ class RLNCNode:
 
 	def receive(self, message):
 		if np.all(message.coefficients==0):
-			return # Message is just the 0 vector
+			return # Message is just the 0 vector and hence adds no value
 		elif self.can_decode():
 			return # No need to receive anything
 
-		if self.messages.size == 0:
-			print message
+		if self.messages.size == 0: # I know nothing, so this has to add value
 			self.coefficients = np.array([message.coefficients])
 			self.messages = np.array([message.message])
 		else:
+			# Add a message if the rank increases (implies new messsage is linearly independent of old ones)
 			old_rank = self.rank()
 			new_coefficients = np.concatenate((self.coefficients, np.array([message.coefficients])), axis=0)
 			new_messages = np.concatenate((self.messages, np.array([message.message])), axis=0)
 			new_rank = compute_rank(new_coefficients)
 			if new_rank > old_rank:
-				print message
 				self.coefficients = new_coefficients
 				self.messages = new_messages
 
@@ -50,8 +54,8 @@ class RLNCNode:
 			return
 
 		rand_coefficients = np.random.randint(0, FIELD_SIZE, self.messages.shape[0]) # One random coefficient for each message I know about
-		message = mod(np.dot(rand_coefficients, self.messages))
-		coefficients = mod(np.dot(rand_coefficients, self.coefficients))
+		message = mod(np.dot(rand_coefficients, self.messages)).astype(np.int)
+		coefficients = mod(np.dot(rand_coefficients, self.coefficients)).astype(np.int)
 		return RLNCMessage(coefficients, message)
 
 	def decode(self):
@@ -59,14 +63,13 @@ class RLNCNode:
 		b = self.messages
 		assert a.shape == (NUM_MESSAGES, NUM_MESSAGES)
 		assert b.shape == (NUM_MESSAGES, MESSAGE_LENGTH)
-		solution = np.linalg.solve(a, b) #Not allowed to do this because of the whole finite field thing
-		solution = mod(solution)
-		return solution.astype(np.int)
+		ma = COEFF_MATRIX_SPACE(list(a.flatten()))
+		inv = ma.inverse()
+		mb = MESSAGE_MATRIX_SPACE(list(b.flatten()))
+		return inv * mb
 
 class RLNCMessage:
 	def __init__(self, coefficients, message):
-		assert coefficients.shape == (NUM_MESSAGES,)
-		assert message.shape == (MESSAGE_LENGTH, )
 		self.coefficients = coefficients
 		self.message = message
 
@@ -74,7 +77,11 @@ class RLNCMessage:
 		return "coefficients:" + str(self.coefficients) + "message:" + str(self.message)
 
 def compute_rank(matrix):
-	return np.linalg.matrix_rank(matrix)
+	if matrix.size == 0:
+		return 0
+	MS = MatrixSpace(GF(FIELD_SIZE), matrix.shape[0], matrix.shape[1])
+	M = MS(list(matrix.astype(np.int).flatten()))
+	return M.rank()
 
 def summarize(graph):
 	for node in graph.nodes():
@@ -86,9 +93,6 @@ def mod(array):
 
 def can_stop_sending(graph):
 	return all([graph.node[node]['rlnc'].can_decode() for node in graph.nodes()])
-
-def generate_complete_graph(n):
-	return nx.complete_graph(n)
 
 def gossip_round(graph, exchange_type=PUSH):
 	for node in graph.nodes():	
@@ -114,34 +118,46 @@ def send_message(sender, receiver):
 
 def verify_solution(solution, messages):
 	for message in messages:
-		assert message.tolist() in solution.tolist()
+		assert message in solution
 
 # TODO: count number of messages sent?
-# TODO: loop for variance
+# TODO: loop for variance (it is a randomized algorithm, afterall)
 
+
+"""
+Graph generators
+"""
+def generate_complete_graph(n):
+	return nx.complete_graph(n)
+
+def generate_path_graph(n):
+	return nx.path_graph(n)
+
+def generate_wheel_graph(n):
+	return nx.wheel_graph(n)
+
+def generate_cycle_graph(n):
+	return nx.cycle_graph(n)
+
+# TODO: random graphs, dynamic graphs
 if __name__ == '__main__':
 	
 	# 1) Generate a connected graph
+	# TODO: determine graph type
 	print "Generating graph"
 	graph = generate_complete_graph(NUM_NODES)
 	for node in graph.nodes():
 		graph.node[node]['rlnc'] = RLNCNode(node)
 
 	# 2) Create the messages
-	messages = [
-		np.array([1, 0,] * 5),
-		np.array([1] * 10),
-		np.array([1] * 9 + [0])
-	]
-	NUM_MESSAGES = len(messages)
-	MESSAGE_LENGTH = len(messages[0])
-	for message in messages:
-		assert len(message) == MESSAGE_LENGTH
+	print "Generating %d messages of length %d in a field of size %d" % (NUM_MESSAGES, MESSAGE_LENGTH, FIELD_SIZE)
+	messages = MESSAGE_MATRIX_SPACE.random_element()
 
 	# 3) Distribute the messages
-	standard_basis_vectors = np.identity(NUM_MESSAGES)
-	for i, message in enumerate(messages):
-		graph.node[0]['rlnc'].receive(RLNCMessage(standard_basis_vectors[i], message))
+	standard_basis_vectors = matrix.identity(NUM_MESSAGES)
+	for i, (standard_basis_vector, message) in enumerate(zip(standard_basis_vectors, messages.rows())):
+		# TODO: the starting configuration should determine how things are spread?
+		graph.node[0]['rlnc'].receive(RLNCMessage(standard_basis_vector, message))
 	
 	# 4) Run gossip until can stop
 	print "Starting protocol"
@@ -157,5 +173,6 @@ if __name__ == '__main__':
 	# 5) Verify answer
 	for node in graph.nodes():
 		solution = graph.node[node]['rlnc'].decode()
-		print solution
 		verify_solution(solution, messages)
+
+	print "Solution has been verified!"
